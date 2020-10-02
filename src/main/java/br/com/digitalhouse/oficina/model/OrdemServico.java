600c8d4f6b08f89e4a2b54a81bba13fe563083ac
Otavio Miranda <otavio.mpm@gmail.com>
2020-10-02 19:04:29 -0300

    OrdemService

---
+++ src/main/java/br/com/digitalhouse/oficina/model/OrdemServico.java
@@ -0,0 +1,97 @@
+package br.com.digitalhouse.oficina.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ordem-servico")
+public class OrdemServico {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@OneToMany
+	@JoinColumn(name ="cliente_id")
+	private Cliente cliente;
+	
+	@OneToMany
+	@JoinColumn (name ="veiculo_id")
+	private Veiculo veiculo;
+	
+	@Column
+	private String descricao;
+
+	public OrdemServico() {
+	}
+
+	public OrdemServico(Long id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemServico other = (OrdemServico) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
